8a55daa4cce86b930786f2c520ead3d5839fd74c
Ashiya Banu <ashiya1005@gmail.com>
2024-01-23 23:47:03 +0530

    get initial data

--- task-craft/src/App.jsx
+++ task-craft/src/App.jsx
@@ -1,6 +1,16 @@
+import { useEffect, useState } from 'react'
 import Header  from './components/Header'
+import jsonData from './assets/tasks.json';
 
 const App = () => {
+  const [data, setData] = useState(null)
+
+  useEffect(() => {
+    let getLocalData = JSON.parse(localStorage.getItem('userData'))
+    // Fetch data from the imported JSON file if no local data is present
+    getLocalData ? setData(getLocalData) : setData(jsonData);
+  }, []);
+
   return (
     <>
       <Header />
