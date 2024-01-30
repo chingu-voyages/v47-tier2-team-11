0a377128b6f9494be1e87366f61d73af01c4b276
Dilip <dilipboidya.office@gmail.com>
2024-01-30 11:49:25 +0530

    create category component

---
+++ task-craft/src/components/Category.jsx
@@ -0,0 +1,32 @@
+import React, { useEffect, useState } from "react";
+// import Activity from "./Activity";
+import "./Category.css";
+const Category = ({ data }) => {
+  const [categoryNames, setCategoryNames] = useState([]);
+
+  useEffect(() => {
+    if (data) {
+      const names = data.map((item) => item.categoryName);
+      setCategoryNames(names);
+    }
+  }, [data]);
+
+  if (!data) {
+    return <p>Please wait</p>;
+  }
+
+  return (
+    <>
+      <ul className="noBullets">
+        {categoryNames.map((name, index) => (
+          <li key={index}>
+            {name}
+            {/* <Activity categoryData={data[index]} /> */}
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+};
+
+export default Category;
