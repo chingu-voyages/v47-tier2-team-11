9cda5afefdc6cea9c7f6ee96095e580124b0552f
KEEN <kennicmwimu@gmail.com>
2024-02-09 17:44:31 +0300

    added the icons and little styling of the header

--- task-craft/src/components/Header.jsx
+++ task-craft/src/components/Header.jsx
@@ -1,7 +1,20 @@
+import logo from '../assets/logo.png';
+import add from '../assets/save-svgrepo-com.svg'
+import save from '../assets/add-circle-svgrepo-com (1).svg'
+import reset from '../assets/reset-svgrepo-com.svg'
+import './Header.css';
 const Header = () => {
     return (
         <>
-            <h1>Task Craft</h1>
+            <div className='container'>
+            <img className='logo' src={logo}/> 
+            <h1 className='head'>Task Craft</h1>
+            <div >
+            <img className='icon-add' src={add}/>
+            <img className='icon-reset'  src={save}/>
+            </div>
+            
+            </div>
         </>
     )
 
