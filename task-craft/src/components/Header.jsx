79160df0cd03629fb766afec88bf0b2ec8293a4e
KEEN <kennicmwimu@gmail.com>
2024-02-13 14:02:28 +0300

    the header component

--- task-craft/src/components/Header.jsx
+++ task-craft/src/components/Header.jsx
@@ -1,10 +1,21 @@
+import logo from "../assets/logo.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+
+import "./Header.css";
+
 const Header = () => {
-    return (
-        <>
-            <h1>Task Craft</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <img className="logo" src={logo} />
+        <h1 className="head">Task Craft</h1>
+        <FontAwesomeIcon className="icon" icon={faPlus} />
+        <FontAwesomeIcon className="icon" icon={faArrowsRotate} />
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
