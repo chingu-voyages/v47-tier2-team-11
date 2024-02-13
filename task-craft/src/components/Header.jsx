2879ac53c87bf497b507ab3866d1ffe7914553c6
KEEN <kennicmwimu@gmail.com>
2024-02-13 07:29:31 +0300

    header with fontawesome

--- task-craft/src/components/Header.jsx
+++ task-craft/src/components/Header.jsx
@@ -1,23 +1,24 @@
-import logo from '../assets/logo.png';
-import add from '../assets/save-svgrepo-com.svg'
-import save from '../assets/add-circle-svgrepo-com (1).svg'
-import reset from '../assets/reset-svgrepo-com.svg'
-import './Header.css';
+import logo from "../assets/logo.png";
+import add from "../assets/save-svgrepo-com.svg";
+import save from "../assets/add-circle-svgrepo-com (1).svg";
+import reset from "../assets/reset-svgrepo-com.svg";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPlus } from "@fortawesome/free-solid-svg-icons";
+import { faArrowsRotate } from "@fortawesome/free-solid-svg-icons";
+
+
+import "./Header.css";
 const Header = () => {
-    return (
-        <>
-            <div className='container'>
-            <img className='logo' src={logo}/> 
-            <h1 className='head'>Task Craft</h1>
-            <div >
-            <img className='icon-add' src={add}/>
-            <img className='icon-reset'  src={save}/>
-            </div>
-            
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="container">
+        <img className="logo" src={logo} />
+        <h1 className="head">Task Craft</h1>
+        <FontAwesomeIcon className="icon" icon={faPlus} />
+        <FontAwesomeIcon className="icon" icon={faArrowsRotate} />
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
