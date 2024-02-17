a165afbafb03772bec41ff88ef3eda68e6350366
Dinesh Anbazhagan <itzmidinesh@gmail.com>
2024-02-17 14:42:25 +0530

    Restructure Header component with basic layout

--- task-craft/src/components/Header.jsx
+++ task-craft/src/components/Header.jsx
@@ -1,10 +1,21 @@
+import logo from "../assets/logo.png";
+import "./Header.css";
+
 const Header = () => {
-    return (
-        <>
-            <h1>Task Craft</h1>
-        </>
-    )
+  return (
+    <>
+      <div className="header">
+        <img className="header-logo" src={logo} alt="logo" />
+        <h1>Task Craft</h1>
+        <button title="Add Task" className="header-button">
+          <i className="fas fa-plus icon"></i>
+        </button>
+        <button title="Reset Tasks" className="header-button">
+          <i class="fas fa-recycle icon"></i>
+        </button>
+      </div>
+    </>
+  );
+};
 
-}
-
-export default Header
+export default Header;
