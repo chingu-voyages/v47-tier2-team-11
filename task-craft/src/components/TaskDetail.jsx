d250838bfd1daa69ad578c04fd56d8326bcc4d14
Ashiya Banu <ashiya1005@gmail.com>
2024-02-26 00:00:02 +0530

    change styles

--- task-craft/src/components/TaskDetail.jsx
+++ task-craft/src/components/TaskDetail.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from "react";
-import Modal from "react-modal";
 import "./TaskDetail.css";
 import { saveToLocalStorage } from "./TaskHandler";
 import setOccurrences from "./Occurrences";
@@ -125,176 +124,176 @@ const TaskDetail = ({
         {taskDetails.taskName}
       </button>
 
-      <Modal
-        className="TaskDetailModalStyles"
-        ariaHideApp={false}
-        isOpen={modalIsOpen}
-        onRequestClose={closeModal}
-        contentLabel="Task Details"
-      >
-        <div>
-          <button className="btnClose" onClick={closeModal}>
-            <i className="fas fa-times"></i>
-          </button>
-          <h2 className="taskDetailHeading">Task Details</h2>
+      {modalIsOpen && (
+        <div className="task-detail-modal-overlay">
+          <div className="task-detail-modal-content">
+            <div className="task-detail-modal-header">
+              <h2 id="detail-modal-title">Task Details</h2>
+              <button className="btnClose" onClick={closeModal}>
+                <i className="fas fa-times"></i>
+              </button>
+            </div>        
 
-          <div className="categoryContainer">
-            Category: {category}
-            <button
-              className="editButton tasknameEditButton"
-              onClick={handleEdit}
-            >
-              <i className="fas fa-edit"></i>
-            </button>
-          </div>
-          <div className="activityContainer">Activity: {activity}</div>
+            <div id="editableContainer">
+            <div className="categoryContainer">
+              Category: {category}
+              <button
+                className="editButton tasknameEditButton"
+                onClick={handleEdit}
+              >
+                <i className="fas fa-edit"></i>
+              </button>
+            </div>
+            <div className="activityContainer">Activity: {activity}</div>
 
-          <div className="tasknamecontainer">
-            <div className="taskname">
-              {" "}
-              Taskname:{" "}
+            <div className="tasknamecontainer">
+              <div className="taskname">
+                {" "}
+                Taskname:{" "}
+                {edit ? (
+                  <input
+                    className="tasknameInputContainer"
+                    type="text"
+                    defaultValue={taskDetails.taskName}
+                    onBlur={handleTaskNameChange}
+                  />
+                ) : (
+                  <> {taskDetails.taskName}</>
+                )}
+              </div>
+            </div>
+            <div className="descriptionContainer">
+              Description:{" "}
               {edit ? (
                 <input
-                  className="tasknameInputContainer"
                   type="text"
-                  defaultValue={taskDetails.taskName}
-                  onBlur={handleTaskNameChange}
+                  className="inputDescriptionContainer"
+                  defaultValue={taskDetails.taskDescription}
+                  onBlur={handleDescriptionChange}
                 />
               ) : (
-                <> {taskDetails.taskName}</>
+                <>{taskDetails.taskDescription}</>
+              )}
+            </div>
+            <div className="taskpriority priorityContainer">
+              Priority:{" "}
+              {edit ? (
+                <select
+                  className="inputPriorityContainer"
+                  defaultValue={taskDetails.priority}
+                  onBlur={handlePriorityChange}
+                >
+                  <option value="default"> --select priority-- </option>
+                  <option value="high">High</option>
+                  <option value="medium">Medium</option>
+                  <option value="low">Low</option>
+                </select>
+              ) : (
+                <>
+                  {taskDetails.priority?.[0].toUpperCase() +
+                    taskDetails.priority?.slice(1)}
+                </>
+              )}
+            </div>
+            <div className="duedateContainer">
+              {"Due Date:"}
+              {edit ? (
+                <div className="editingDuedateContainer">
+                  <div>
+                    <input
+                      type="radio"
+                      id="daily"
+                      name="dueDate"
+                      value="daily"
+                      checked={dueDateType === "daily"}
+                      onChange={() => handleDueDateTypeChange("daily")}
+                    />
+                    <label htmlFor="daily">Daily</label>
+                  </div>
+                  <div>
+                    <input
+                      type="radio"
+                      id="weekly"
+                      name="dueDate"
+                      value="weekly"
+                      checked={dueDateType === "weekly"}
+                      onChange={() => handleDueDateTypeChange("weekly")}
+                    />
+                    <label htmlFor="weekly">Weekly</label>
+                    {dueDateType === "weekly" && (
+                    <select
+                      className="inputWeeklyContainer"
+                      value={editedDueday}
+                      onChange={handleChangeDay}
+                      onBlur={handleChangeDay}
+                    >
+                      <option value="">Select Day</option>
+                      <option value="monday">Monday</option>
+                      <option value="tuesday">Tuesday</option>
+                      <option value="wednesday">Wednesday</option>
+                      <option value="thursday">Thursday</option>
+                      <option value="friday">Friday</option>
+                      <option value="saturday">Saturday</option>
+                      <option value="sunday">Sunday</option>
+                    </select>
+                  )}
+                  </div>
+
+                  <div>
+                    <input
+                      type="radio"
+                      id="onetime"
+                      className="onetimeRadio"
+                      name="dueDate"
+                      value="onetime"
+                      checked={dueDateType === "onetime"}
+                      onChange={() => handleDueDateTypeChange("onetime")}
+                    />
+                    <label htmlFor="onetime">One-time</label>
+                    {dueDateType === "onetime" && (
+                    <input
+                      className="inputOneTimeContainer"
+                      type="date"
+                      value={editedDuedate || format(new Date(), "yyyy-MM-dd")}
+                      onChange={handleChangeDate}
+                    />
+                  )}
+                  </div>
+                </div>
+              ) : (
+                <>
+                  {" "}
+                  {taskDetails.day === "daily" ? (
+                    <span>
+                      Repeats{" "}
+                      {taskDetails.day?.[0].toUpperCase() +
+                        taskDetails.day?.slice(1)}
+                    </span>
+                  ) : taskDetails.repetition === true ? (
+                    <span>
+                      Repeats on{" "}
+                      {taskDetails.day?.[0].toUpperCase() +
+                        taskDetails.day?.slice(1)}
+                    </span>
+                  ) : (
+                    <span>
+                      On{" "} {taskDetails.date}
+                    </span>
+                  )}
+                </>
+              )}
+            </div>
+            </div>
+            <div className="saveButtonContainer">
+              {edit && (
+                <button className="saveBtn" onClick={handleSave}>
+                  Save
+                </button>
               )}
             </div>
           </div>
-          <div className="descriptionContainer">
-            Description:{" "}
-            {edit ? (
-              <input
-                type="text"
-                className="inputDescriptionContainer"
-                defaultValue={taskDetails.taskDescription}
-                onBlur={handleDescriptionChange}
-              />
-            ) : (
-              <>{taskDetails.taskDescription}</>
-            )}
-          </div>
-          <div className="taskpriority priorityContainer">
-            Priority:{" "}
-            {edit ? (
-              <select
-                className="inputPriorityContainer"
-                defaultValue={taskDetails.priority}
-                onBlur={handlePriorityChange}
-              >
-                <option value="default"> --select priority-- </option>
-                <option value="high">High</option>
-                <option value="medium">Medium</option>
-                <option value="low">Low</option>
-              </select>
-            ) : (
-              <>
-                {taskDetails.priority?.[0].toUpperCase() +
-                  taskDetails.priority?.slice(1)}
-              </>
-            )}
-          </div>
-          <div className="duedateContainer">
-            {"Due Date:"}
-            {edit ? (
-              <div className="editingDuedateContainer">
-                <div>
-                  <input
-                    type="radio"
-                    id="daily"
-                    // className="dailyRadio"
-                    name="dueDate"
-                    value="daily"
-                    checked={dueDateType === "daily"}
-                    onChange={() => handleDueDateTypeChange("daily")}
-                  />
-                  <label htmlFor="daily">Daily</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="weekly"
-                    name="dueDate"
-                    value="weekly"
-                    checked={dueDateType === "weekly"}
-                    onChange={() => handleDueDateTypeChange("weekly")}
-                  />
-                  <label htmlFor="weekly">Weekly</label>
-                </div>
-                <div>
-                  <input
-                    type="radio"
-                    id="onetime"
-                    className="onetimeRadio"
-                    name="dueDate"
-                    value="onetime"
-                    checked={dueDateType === "onetime"}
-                    onChange={() => handleDueDateTypeChange("onetime")}
-                  />
-                  <label htmlFor="onetime">One-time</label>
-                </div>
-                {dueDateType === "onetime" && (
-                  <input
-                    className="inputOneTimeContainer"
-                    type="date"
-                    value={editedDuedate || format(new Date(), "yyyy-MM-dd")}
-                    onChange={handleChangeDate}
-                  />
-                )}
-                {dueDateType === "weekly" && (
-                  <select
-                    className="inputWeeklyContainer"
-                    value={editedDueday}
-                    onChange={handleChangeDay}
-                    onBlur={handleChangeDay}
-                  >
-                    <option value="">Select Day</option>
-                    <option value="monday">Monday</option>
-                    <option value="tuesday">Tuesday</option>
-                    <option value="wednesday">Wednesday</option>
-                    <option value="thursday">Thursday</option>
-                    <option value="friday">Friday</option>
-                    <option value="saturday">Saturday</option>
-                    <option value="sunday">Sunday</option>
-                  </select>
-                )}
-              </div>
-            ) : (
-              <>
-                {" "}
-                {taskDetails.day === "daily" ? (
-                  <span>
-                    Repeats{" "}
-                    {taskDetails.day?.[0].toUpperCase() +
-                      taskDetails.day?.slice(1)}
-                  </span>
-                ) : taskDetails.repetition === true ? (
-                  <span>
-                    Repeats on{" "}
-                    {taskDetails.day?.[0].toUpperCase() +
-                      taskDetails.day?.slice(1)}
-                  </span>
-                ) : (
-                  <span>
-                    On{" "} {taskDetails.date}
-                  </span>
-                )}
-              </>
-            )}
-          </div>
-          <div className="saveButtonContainer">
-            {edit && (
-              <button className="saveBtn" onClick={handleSave}>
-                Save
-              </button>
-            )}
-          </div>
         </div>
-      </Modal>
+      )}
     </>
   );
 };
